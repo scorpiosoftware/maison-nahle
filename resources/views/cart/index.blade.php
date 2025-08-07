bdd0a33786f606f80e8180c0cda848e35a1a5f12
Scorpio Software <scorpiosoftware666@gmail.com>
2025-08-07 21:41:10 +0300

    size lable to cart page

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -41,7 +41,7 @@
                                                         style="background-color: {{ $details['color']->hex_code ?? '' }};">
                                                     </div>
                                                 @endif
-                                                <div class=" rounded-full size-6">{{ $details['size']->name }}</div>
+                                                <div class=" rounded-full size-6"> size : {{ $details['size']->name }}</div>
                                             </div>
 
                                             <div class="flex items-center gap-4">
